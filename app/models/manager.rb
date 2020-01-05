b919439bc51f0affe6981082be14b1c61bf0a0b4
Ivan Denisov <johnny.denisov@gmail.com>
2020-01-05 19:18:04 +0100

    add missing Manager.from_omniauth method

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -2,4 +2,10 @@ class Manager < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise :omniauthable, omniauth_providers: %i[facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+    end
+  end
 end
