f938454310eb2d0b09258b361f9dc541e53a0c43
Ivan Denisov <johnny.denisov@gmail.com>
2020-01-05 19:19:02 +0100

    fix sign out for two models

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :managers#, controllers: { omniauth_callbacks: 'managers/omniauth_callbacks' }
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    delete "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+    delete "users/sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
   end
   devise_scope :manager do
-    delete "sign_out", to: "devise/sessions#destroy", as: :destroy_manager_session
+    delete "managers/sign_out", to: "devise/sessions#destroy", as: :destroy_manager_session
   end
 
   root to: "landing#index"
